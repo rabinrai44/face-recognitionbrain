e06e2e018691789f1810b6780b55ef714a283a9a
Rabin Rai <rabinrai44@gmail.com>
2018-01-30 22:15:21 -0500

    Customized particles option.

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import './App.css';
 
 const particlesOptions = {
   particles: {
-    line_linked: {
-      shadow: {
+    number: {
+      value: 30,
+      density: {
         enable: true,
-        color: "#3CA9D1",
-        blur: 5
+        value_area: 800
       }
     }
   }
