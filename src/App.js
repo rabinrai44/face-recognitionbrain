bbaa49a1d8444c1c9687a8069ba5a345b594ead9
Rabin Rai <rabinrai44@gmail.com>
2018-01-30 22:12:29 -0500

    Added Rank component and particles-react for bg interactive.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+         <div className="particles">
+            <Particles params={particlesOptions} />
+          </div>
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
        {/* <FaceRecognition /> */}
       </div>
